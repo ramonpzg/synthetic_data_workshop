3c799d75929e0b53a037b9f20e6f5ce5c585329d
Ramon Perez <ramonpzg@protonmail.com>
2023-10-30 05:03:34 -0700

    ignore and main.py app added

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="./templates")
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/service1")
+async def read_page2(request: Request):
+    return templates.TemplateResponse("page1.html", {"request": request})
